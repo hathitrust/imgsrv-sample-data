70cd4be4c9e5d39fa6214810053b0c14a42ab7cb
Roger Espinosa <roger@umich.edu>
2020-09-18 16:53:10 -0400

    fix dbh

--- bin/load_sample.pl
+++ bin/load_sample.pl
@@ -17,10 +17,11 @@ my $config = new MdpConfig("$ENV{SDRROOT}/mdp-lib/Config/uber.conf");
 $C->set_object('MdpConfig', $config);
 
 my $db = new Database('root', 'TIMTOWTDIBSCINABTE', 'ht_rights', 'mariadb');
+my $dbh = $db->get_DBH();
+
 open my $fh, '<', "$FindBin::Bin/../sample_data.sql" or die "Cannot open file $!";
-# my $sql = do { local $/; <$fh> };
 while ( my $sql = <$fh> ) {
     chomp $sql;
     next unless ( $sql );
-    $db->do($sql) or warn DBI->errstr;
+    $dbh->do($sql) or warn DBI->errstr;
 }
