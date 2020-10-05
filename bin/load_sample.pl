345b769933e3dcc45397db6fe333cb20fe37257b
Roger Espinosa <roger@umich.edu>
2020-10-05 11:54:31 -0400

    do not use ht_rights

--- bin/load_sample.pl
+++ bin/load_sample.pl
@@ -16,8 +16,13 @@ $C->set_object('CGI', $cgi);
 my $config = new MdpConfig("$ENV{SDRROOT}/mdp-lib/Config/uber.conf");
 $C->set_object('MdpConfig', $config);
 
-my $db = new Database('root', 'TIMTOWTDIBSCINABTE', 'ht_rights', 'mariadb');
+my $db = new Database('root', 'TIMTOWTDIBSCINABTE', 'ht', 'mariadb');
 my $dbh = $db->get_DBH();
+
+my $check = $dbh->selectrow_hashref(qq{SELECT * FROM rights_current WHERE namespace = ? AND id = ?}, undef, 'loc', 'ark:/13960/t5v69h717');
+if ( ref($check) && $$check{id} ) {
+    exit;
+}
 
 open my $fh, '<', "$FindBin::Bin/../sample_data.sql" or die "Cannot open file $!";
 while ( my $sql = <$fh> ) {
