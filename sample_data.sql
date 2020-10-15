a2b3690cb48a5d0789080aaac8bac5083cf43d9e
Roger Espinosa <roger@umich.edu>
2020-10-15 15:31:07 -0400

    use fixture identifiers

--- sample_data.sql
+++
@@ -1,3 +0,0 @@
-LOCK TABLES `rights_current` WRITE;
-INSERT INTO `rights_current` (`namespace`, `id`, `attr`, `reason`, `source`, `access_profile`, `user`, `time`, `note`) VALUES ('loc', 'ark:/13960/t5v69h717', 1, 1, 4, 1, 'timothy', '2011-03-20 19:31:14', NULL);
-UNLOCK TABLES;
